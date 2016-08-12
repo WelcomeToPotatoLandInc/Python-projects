9f8c2c92565de5947e30264968af672415eae918
WelcomeToPotatoLandInc <thewolfington@gmail.com>
2016-08-12 16:24:24 +0200

    Add files via upload

---
+++ pokerman.py
@@ -0,0 +1,16 @@
+pokerman=[
+    "sausage",
+    "pidgy",
+    "peekachorp",
+    "electric bird",
+    "eggs",
+    "bat",
+    "fat purple ball thats high on weed or something"]
+print("hey kid wanna hear some pokerman?")
+print(pokerman[1:3])
+print(pokerman[:])
+print(pokerman[:-1])
+print("and thats all.")
+# no it isnt
+# yes it is
+#no it isnt, because im a code expert, pressing my keyboard when my computers not resting. please forgive me for this if you are using this script
